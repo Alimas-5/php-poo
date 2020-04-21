56e75f23241cf6aba56ca3d4cc69d043f97daac3
unknown <ludovic.bourget@live.fr>
2020-04-21 15:43:28 +0200

    rendu fin de 21/04

--- 3-php-training-mysql/read.php
+++ 3-php-training-mysql/read.php
@@ -7,5 +7,41 @@
   <body>
     <h1>Liste des jeux de société</h1>
     <!-- Afficher la liste des jeux -->
+    <?php
+    include "inc/DBConnection.php";
+    include "inc/Boardgame.php";
+
+    $bdd = DBConnection::getInstance()->getConnection();
+    $boardgames = $bdd->query('SELECT * FROM boardgames')->fetchAll();
+    foreach ($boardgames as $key => $game) {
+      $boardgame= new Boardgame($game);
+      echo "<br/>";
+      echo "Nom du jeu : ";
+  
+      echo $game[1];
+      echo "<br/>";
+      echo "<img src='$game[6]'>";
+      echo "<br/>";
+      echo "Nombre de joueur minimum : ";
+      echo $game[2] ;
+      echo "<br/>";
+  
+      echo "Nombre de joueur maximum : ";
+      echo $game[3] ;
+      echo "<br/>";
+  
+      echo "Age minimum du joueur : ";
+      echo $game[4] ;
+      echo "<br/>";
+  
+      echo "Age maximum du joueur : ";
+      echo $game[5];
+      echo "<br/>"; 
+  
+      echo "Image du jeu : ";
+      echo $game[6];
+      echo "<br/>";
+    }
+    ?>
   </body>
 </html>
